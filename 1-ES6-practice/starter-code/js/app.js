2c0453843d8c744d990a1935725e095be5ff10b1
Vince Masten <vmasten@me.com>
2018-08-13 11:44:49 -0700

    steps 10-11 complete

--- 1-ES6-practice/starter-code/js/app.js
+++ 1-ES6-practice/starter-code/js/app.js
@@ -16,7 +16,7 @@ const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -34,7 +34,7 @@ function displayPics(){
       viewed.push(rando);
     }
   }
-  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
+  // In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
   // Because rando was changed to let, its scope is now confined within the block of the while loop, so even though the log occurs within the function, it occurs too late to be successful
   console.log(viewed)
 
@@ -61,7 +61,7 @@ function handleClick(event) {
   for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -72,7 +72,7 @@ function handleClick(event) {
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`;
     list.appendChild(liEl);
   }
 }
